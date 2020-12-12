650b722230b80b024cc280104281cd08183cbaca
deb <deborah.chrystall.djon@outlook.com>
2020-12-12 18:30:02 +0100

    Added form to the add Guest page

--- src/app/pages/new-guest/new-guest.component.ts
+++ src/app/pages/new-guest/new-guest.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-new-guest',
   templateUrl: './new-guest.component.html',
-  styleUrls: ['./new-guest.component.css']
+  styleUrls: ['./new-guest.component.less']
 })
 export class NewGuestComponent implements OnInit {
+  @Input() firstName: any;
+  @Input() lastName: any;
+  @Input() fromTime: any;
+  @Input() toTime: any;
+  @Input() date: any;
+  @Input() roomNumber: any;
+  @Input() phoneNumber: any;
+  @Input() emailAddress: any;
+  @Input() street: any;
+  @Input() houseNumber: any;
+  @Input() county: any;
+  @Input() zipCode: any;
+  @Input() country: any;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+
+  }
 }
