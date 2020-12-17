87639a2fca796d8ce89b603dc288505d2c8a662b
deb <deborah.chrystall.djon@outlook.com>
2020-12-18 00:22:27 +0100

    Added alarm functionality for guests Created service for passing data from finding guests/staff page to the contact list page

---
+++ src/app/services/contact-list/contact-list.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {GuestwId, StaffwId} from "../../../typescript-angular-client-generated";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactListService {
+
+  public guests: GuestwId[] = [];
+  public staff: StaffwId[] = [];
+
+  constructor() {return}
+
+  addContacts(guests: GuestwId[], staff: StaffwId[]): void {
+    this.guests = guests;
+    this.staff = staff;
+    console.log(this.guests)
+  }
+
+  removeContacts(): void {
+    this.guests = [];
+    this.staff = [];
+  }
+
+  public getGuests(): GuestwId[] {
+    return this.guests;
+  }
+
+  public getStaff(): StaffwId[] {
+    return this.staff;
+  }
+
+}
